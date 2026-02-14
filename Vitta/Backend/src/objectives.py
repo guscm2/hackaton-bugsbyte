292aebbd23b4d497755ab98dae709b7b4d683f32
Guscm2 <Gustavocmartins2@gmail.com>
2026-02-14 11:41:21 +0000

    resto das classes adicionadas

---
+++ Vitta/Backend/src/objectives.py
@@ -0,0 +1,35 @@
+from datetime import date 
+
+today = date.today()
+class Objective:
+    def __init__ (self, name, atual, value, status = "In Progress"):
+        self.name = name
+        self.atual = atual
+        self.value = value
+        self.status = status
+
+    def get_name(self):
+        return self.name
+    
+    def get_status(self):
+        return self.status
+    
+    def get_atual(self):
+         return self.atual
+
+    def set_atual(self, atual):
+        self.atual = atual
+    
+    def set_value(self, value):
+        self.value = value
+    
+    def update_objective(self):
+        self.atual += 1
+        if self.name != "Peso":
+            if today.isoweekday() == 7:
+                self.atual = 0
+
+    def completed_objective(self):
+        if self.atual >= self.value:
+            self.status = "Completed"
+    

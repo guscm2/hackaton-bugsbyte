36c7b2c4721135738813f8f2c6de5b5501c4eac1
Guscm2 <Gustavocmartins2@gmail.com>
2026-02-14 00:01:35 +0000

    streak classes update

--- Vitta/Backend/src/Streak.py
+++ Vitta/Backend/src/Streak.py
@@ -1,25 +1,31 @@
-from datetime import date 
+from datetime import date, timedelta
 
 class Streak :
-    def innit(self):
+    def innit(self, freezes = 0):
         self.count = 0
         self.last_completed = None
-        self.streak_freezes = 0
+        self.streak_freezes = freezes
 
     def reset_streak(self):
         self.count = 0
-        self.last_completed = None
 
     def update_streak(self):
         today = date.today()
-        if self.last_completed == today:
-            return
-        if self.last_completed - today == 1:
-            self.count += 1
-        if self.last_completed - today > 1 and self.streak_freezes <= 0:
-            self.reset_streak()
-        else:
+
+        if self.last_completed is None:
             self.count = 1
+            self.last_completed = today
+            return
+        delta_days = (today - self.last_completed).days
+
+        if delta_days == 0:
+            return
+        if delta_days == 1:
+            self.count += 1
+        elif delta_days <= 2 and self.streak_freezes > 0:
+            self.streak_freezes -= 1
+
+        else:
+            self.reset_streak()
+
         self.last_completed = today
-
-
